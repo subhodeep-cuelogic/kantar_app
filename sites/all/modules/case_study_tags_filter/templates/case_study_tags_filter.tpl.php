402a28f2079d8e2753146bd9afafd0493b54ee55
subhodeep-cuelogic <subhodeep.majumder@cuelogic.co.in>
2014-09-24 21:35:41 +0530

    implemented the sidebar blocks

---
+++ sites/all/modules/case_study_tags_filter/templates/case_study_tags_filter.tpl.php
@@ -0,0 +1,35 @@
+<?php //echo "<pre>";print_r($arrCaseStudiesListData);die; ?>
+
+<script type="text/javascript">
+
+    jQuery(document).ready(function(){
+        jQuery('#case_study_tags_filter').change(function() { 
+            jQuery("#CaseStudyFilterFrm").submit();
+        });
+    }); 
+
+</script>
+
+
+
+
+<div class="wrapper" align="center">
+<form name="CaseStudyFilterFrm" id="CaseStudyFilterFrm" method="get">
+<select name="case_study_tags_filter" id="case_study_tags_filter" >
+<option value="All" <?php if(isset($_REQUEST['case_study_tags_filter']) &&  ($_REQUEST['case_study_tags_filter']== "All")) { echo "selected"; } ?> >All</option>
+<?php if (is_array($arrTaxonomyList) && count($arrTaxonomyList)>0) {
+        $i = 1;
+        $className = '';
+        
+        foreach ($arrTaxonomyList as $arrTaxonomyListValue) {
+            
+            ?>
+            <option value="<?php echo $arrTaxonomyListValue->tid; ?>" <?php if(isset($_REQUEST['case_study_tags_filter']) && ($_REQUEST['case_study_tags_filter'] == $arrTaxonomyListValue->tid)) { echo "selected"; } ?> ><?php echo $arrTaxonomyListValue->name; ?></option>
+            <?php
+            $i++;
+        }
+    }
+    ?>
+  </select>
+</form>         
+ </div> 
---
+++ sites/all/modules/news_tags_filter/templates/news_tags_filter.tpl.php
@@ -0,0 +1,35 @@
+<?php //echo "<pre>";print_r($arrCaseStudiesListData);die; ?>
+
+<script type="text/javascript">
+
+    jQuery(document).ready(function(){
+        jQuery('#news_tags_filter').change(function() { 
+            jQuery("#NewsFilterFrm").submit();
+        });
+    }); 
+
+</script>
+
+
+
+
+<div class="wrapper" align="center">
+<form name="NewsFilterFrm" id="NewsFilterFrm" method="get">
+<select name="news_tags_filter" id="news_tags_filter" >
+<option value="All" <?php if(isset($_REQUEST['news_tags_filter']) &&  ($_REQUEST['news_tags_filter']== "All")) { echo "selected"; } ?> >All</option>
+<?php if (is_array($arrTaxonomyList) && count($arrTaxonomyList)>0) {
+        $i = 1;
+        $className = '';
+        
+        foreach ($arrTaxonomyList as $arrTaxonomyListValue) {
+            
+            ?>
+            <option value="<?php echo $arrTaxonomyListValue->tid; ?>" <?php if(isset($_REQUEST['news_tags_filter']) && ($_REQUEST['news_tags_filter'] == $arrTaxonomyListValue->tid)) { echo "selected"; } ?> ><?php echo $arrTaxonomyListValue->name; ?></option>
+            <?php
+            $i++;
+        }
+    }
+    ?>
+  </select>
+</form>         
+ </div> 
